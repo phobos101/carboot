4ade98cdc99ce4d2e75ce9c1fc7d29c759c90f86
phobos101 <robwilson101@gmail.com>
2015-11-09 20:35:37 +0000

    AWS

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,2 @@
-Paperclip::Attachment.default_options[:url] = 's3-website-eu-west-1.amazonaws.com'
+Paperclip::Attachment.default_options[:url] = 'wdi16-b-rob.s3-website-eu-west-1.amazonaws.com'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
